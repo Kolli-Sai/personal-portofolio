472306a53fd6ffd469ab9ab0fa0065c57313fecc
KolliSai <saik98187@gmail.com>
2023-10-13 17:22:22 +0530

    add shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 const HomePage = () => {
   return (
-    <div className=' text-indigo-600'>HomePage</div>
-  )
-}
+    <>
+      <div className=" text-indigo-600">HomePage</div>
+      <Button>Click me</Button>
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
